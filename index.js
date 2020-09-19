1557a0777552b0104004966af8280d21f53583dd
flaribbit <1149761294@qq.com>
2020-09-19 15:31:42 +0800

    气泡绘制初步

--- index.js
+++ index.js
@@ -37,29 +37,45 @@ function random(a, b) {
 function visual(data) {
     var bubbles;
     var currentTime;
-    var names = [];
-    console.log(names);
     console.log(data);
     var i = 0;
-    setInterval(() => {
-        currentTime = data.key[i];
+    setTimeout(() => {
+        currentTime = data.time[i];
         bubbles = data[currentTime].map(e => {
             return {
                 time: currentTime,
                 name: e.name,
+                value: Number(e.value) / 10000,
                 x: random(0, 1280),
                 y: random(0, 720),
-                color: data.color[e.name]
+                color: data.color[e.name],
             }
         });
         update(bubbles);
         draw(bubbles);
         i++;
-    });
+    }, 10);
 }
 
 function draw(bubbles) {
     var svg = d3.select("svg");
+    var update = svg.selectAll("circle").data(bubbles, d => d.name);
+    var enter = update.enter();
+    var exit = update.exit();
+    enter
+        .append("circle")
+        .attr("class", "bubble")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("fill", d => d.color)
+        .transition()
+        .duration(1000)
+        .attr("r", d => d.value);
+    enter.append("text")
+        .attr("class", "name")
+        .attr("x", d => d.x)
+        .attr("y", d => d.y)
+        .text(d => d.name)
 }
 
 function update(bubbles) {
