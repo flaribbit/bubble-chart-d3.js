d06f5f775eb78d295d389eed134bc81119f26771
flaribbit <1149761294@qq.com>
2020-09-19 17:30:05 +0800

    大小变化基本实现

--- index.js
+++ index.js
@@ -59,44 +59,63 @@ function visual(data) {
 
 function draw(bubbles) {
     var svg = d3.select("svg");
-    var update = svg.selectAll("circle").data(bubbles, d => d.name);
-    var enter = update.enter();
+    var update = svg.selectAll("g").data(bubbles, d => d.name);
     var exit = update.exit();
-    enter
-        .append("circle")
+    var enter = update.enter()
+        .append("g")
+        .attr("class", "bubble");
+    //气泡开始
+    enter.append("circle")
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
         .attr("fill", d => d.color)
         .transition()
         .duration(1000)
         .attr("r", d => d.value);
+    //文字标题
     enter.append("text")
         .attr("class", "name")
         .attr("x", d => d.x)
         .attr("y", d => d.y)
         .text(d => d.name);
+    //值
     enter.append("text")
         .attr("class", "value")
         .attr("x", d => d.x)
         .attr("y", d => d.y + 10)
-        .text(d => Math.round(d.value));
-    update
+        .transition()
+        .duration(1000)
+        .tween("text", function (d) {
+            var i = d3.interpolate(0, d.value);
+            return t => this.textContent = i(t).toFixed(3);
+        });
+
+    update.select("circle")
         .transition()
         .duration(1000)
         .attr("r", d => d.value);
-    svg.selectAll("text.value").data(bubbles, d => d.name)
+    update.select(".value")
         .transition()
         .duration(1000)
         .tween("text", function (d) {
             var i = d3.interpolate(this.textContent, d.value);
             return t => this.textContent = i(t).toFixed(3);
         });
-    exit
+
+    exit.select("circle")
         .transition()
         .duration(1000)
-        .attr("r", 0)
-        .remove();
-    svg.selectAll("text.name").data(bubbles, d => d.name).exit()
+        .attr("r", 0);
+    exit.select(".value")
+        .transition()
+        .duration(1000)
+        .tween("text", function () {
+            var i = d3.interpolate(this.textContent, 0);
+            return t => this.textContent = i(t).toFixed(3);
+        });
+    exit
+        .transition()
+        .duration(1100)
         .remove();
 }
 
